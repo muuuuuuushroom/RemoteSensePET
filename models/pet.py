correct box1 normalization

models/pet.py
@@ -338,8 +338,8 @@ class BasePETCount(nn.Module):
             # tmp = self.bbox_embed(hs)
             # tmp += refer_bsig
             # outputs_offsets = tmp.sigmoid()
-            # points_queries[..., 0] /= img_h
-            # points_queries[..., 1] /= img_w            
+            points_queries[..., 0] /= img_h
+            points_queries[..., 1] /= img_w            
             # points_queries = outputs_coord + points_queries
         elif self.opt_query_con:
             refer = kwargs['refer'][-1]
@@ -351,11 +351,12 @@ class BasePETCount(nn.Module):
                 outputs_coord = tmp.sigmoid()
                 outputs_coords.append(outputs_coord)
             outputs_offsets = torch.stack(outputs_coords)
+            points_queries[:, 0] /= img_h
+            points_queries[:, 1] /= img_w
         else:
             outputs_offsets = (self.coord_embed(hs).sigmoid() - 0.5) * 2.0
-            
-        points_queries[:, 0] /= img_h
-        points_queries[:, 1] /= img_w
+            points_queries[:, 0] /= img_h
+            points_queries[:, 1] /= img_w
 
         # rescale offset range during testing
         if 'test' in kwargs: